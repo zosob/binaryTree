ad57345d43debc3428087477fbcd9da8d5115088
Bhaskar Ghosh <bhaskar4647@gmail.com>
2021-02-28 22:35:34 -0600

    Adding examples

--- nodeDel.py
+++ nodeDel.py
@@ -24,9 +24,7 @@ def deleteNode(root, key):
             return root.left
         if not root.left:
             return root.right
-
         #Replace minimum value from right tree and remove from right tree
-
         temp = root.right
         min_val = temp.val
         while temp.left:
@@ -34,10 +32,23 @@ def deleteNode(root, key):
             min_val = temp.val
         root.right = deleteNode(root.right, root.val)
     return root
-    
+
 def preOrder(node):
     if not node:
         return
     print(node.val)
     preOrder(node.left)
     preOrder(node.right)
+
+root = TreeNode(8)
+root.left = TreeNode(5)
+root.right = TreeNode(14)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(6)
+root.left.right.left = TreeNode(8)
+
+print("Original Node:")
+print(preOrder(root))
+result = deleteNode(root, 5)
+print("After deletion:")
+print(preOrder(result))
