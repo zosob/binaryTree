11bf739e50896f2f2f69d565f9665de08aa7c0df
Bhaskar Ghosh <bhaskar4647@gmail.com>
2021-02-28 21:34:03 -0600

    Adding examples

--- closestValue.py
+++ closestValue.py
@@ -9,7 +9,7 @@ class TreeNode(object):
         self.right = None
 
 
-#Defining closest value 
+#Defining closest value
 def closestValue(root, target):
     a = root.val
     child = root.left if target<a else root.right
@@ -17,3 +17,16 @@ def closestValue(root, target):
         return a
     b = closestValue(child, target)
     return min((a,b), key=lambda x: abs(target-x))
+
+root = TreeNode(8)
+root.left = TreeNode(5)
+root.right = TreeNode(14)
+root.left.left = TreeNode(4)
+root.left.right = TreeNode(6)
+root.left.right.left = TreeNode(8)
+root.left.right.right = TreeNode(7)
+root.right.right = TreeNode(24)
+root.right.right.left = TreeNode(22)
+
+result = closestValue(root, 3)
+print(result)
