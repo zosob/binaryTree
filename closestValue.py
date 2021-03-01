3430c900fb4393fac79c71e816087ceaae5d36d2
Bhaskar Ghosh <bhaskar4647@gmail.com>
2021-02-28 21:17:04 -0600

    Creating tree.

---
+++ closestValue.py
@@ -0,0 +1,9 @@
+#Find the closest value of a target in a given non-empty
+#Binary Search Tree (BST) of unique values
+
+#Describing tree 
+class TreeNode(object):
+    def __init__(self,x):
+        self.val = x
+        self.left = None
+        self.right = None
