64e4693e27169ac2cc31bf6da019f420a4d512d0
Bhaskar Ghosh <bhaskar4647@gmail.com>
2021-02-28 21:24:20 -0600

    Closest value definined

--- closestValue.py
+++ closestValue.py
@@ -1,9 +1,19 @@
 #Find the closest value of a target in a given non-empty
 #Binary Search Tree (BST) of unique values
 
-#Describing tree 
+#Describing tree
 class TreeNode(object):
     def __init__(self,x):
         self.val = x
         self.left = None
         self.right = None
+
+
+#Defining closest value 
+def closestValue(root, target):
+    a = root.val
+    child = root.left if target<a else root.right
+    if not child:
+        return a
+    b = closestValue(child, target)
+    return min((a,b), key=lambda x: abs(target-x))
